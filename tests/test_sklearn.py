2522d8fac562759583725fb4d823f045e1fb0a69
Alexandre Rozier <alexandre.rozier@rte-france.com>
2021-09-01 15:45:41 +0200

    Add classification

--- tests/test_sklearn.py
+++ tests/test_sklearn.py
@@ -1,12 +1,19 @@
-from fastml.sklearn import load_boston
-from fastml.sklearn import train_test_split
+from sklearn.datasets import load_boston, load_iris
+from sklearn.model_selection import train_test_split
 
-from fastml.sklearn import cv_regression, get_best_model
+from warML.sklearn.utils import cv_regression, get_best_model, DEFAULT_CLASSIFICATION_MODELS
 
 
-def test_cv():
+def test_sklearn_reg():
     X, y = load_boston(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=8675309)
-    best_models, cv_results = cv_regression(X_train, y_train)
+    best_models, cv_results = cv_regression(X_train, y_train, scoring='neg_mean_squared_error')
+    entry = get_best_model(best_models, cv_results)
+    assert entry is not None
+
+def test_sklearn_classif():
+    X, y = load_iris(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=8675309)
+    best_models, cv_results = cv_regression(X_train, y_train, models=DEFAULT_CLASSIFICATION_MODELS, scoring=['f1_weighted','f1_micro','f1_macro'])
     entry = get_best_model(best_models, cv_results)
     assert entry is not None
---
+++ warML/sklearn/utils.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from scipy.stats import loguniform
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.svm import SVC, SVR
+
+from xgboost import XGBRegressor, XGBClassifier
+
+DEFAULT_REGRESSION_MODELS = [
+    ('KNN', KNeighborsRegressor(), dict(n_neighbors=[2, 4, 8, 16, 32, 64, 128, 256])),
+    ('SVR', SVR(), dict(
+        C=loguniform(1e-4, 1e4),
+        gamma=loguniform(1e-4, 1e4))),
+    ('XGB', XGBRegressor(), dict(n_estimators=[100, 1000]))
+]
+
+DEFAULT_CLASSIFICATION_MODELS = [
+    ('LogReg', LogisticRegression(), dict(C=loguniform(1e-4, 1e4))),
+    ('RF', RandomForestClassifier(), dict(n_estimators=[100, 1000])),
+    ('KNN', KNeighborsClassifier(), dict(leaf_size=[15, 30])),
+    ('SVM', SVC(), dict(
+        C=loguniform(1e-4, 1e4),
+        gamma=loguniform(1e-4, 1e4))),
+    ('GNB', GaussianNB(), dict(var_smoothing=[1e-9])),
+    ('XGB', XGBClassifier(), dict(n_estimators=[100, 1000]))
+]
+
+
+def cv_regression(X_train: pd.DataFrame, y_train: pd.DataFrame, models=DEFAULT_REGRESSION_MODELS, **kwargs):
+    dfs = []
+
+    best_models = dict()
+    for name, model, distributions in models:
+        random_search = RandomizedSearchCV(model,
+                                           distributions,
+                                           n_iter=20,
+                                           refit=True,
+                                           n_jobs=None,
+                                           random_state=666,
+                                           **kwargs)
+        random_search.fit(X_train, y_train)
+
+        this_df = pd.DataFrame(random_search.cv_results_)
+        this_df['model'] = name
+        best_models[name] = random_search.best_estimator_
+        dfs.append(this_df)
+    return best_models, pd.concat(dfs, ignore_index=True)
+
+
+def get_best_model(best_models, cv_results):
+    idx = cv_results['mean_test_score'].idxmax()
+    best_entry = cv_results.loc[idx].dropna()
+    best_model = best_models[best_entry.model]
+    return best_model
